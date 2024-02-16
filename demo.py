c7ed5d8486732919f0dadcddc291bb40df3ed432
anantwithluna <157704810+anantwithluna@users.noreply.github.com>
2024-02-17 00:57:48 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,22 +1,38 @@
 import streamlit as st
-st.header("Luna's NER demo")
+st.title("Luna Vista")
+st.sidebar.image('demo.jpg')
 st.sidebar.header("Link to Demo Document")
 st.sidebar.link_button("Read the Demo Doc", "https://www.youtube.com/watch?v=TU1gMloI0kc&pp=ygUMb3BlbiBhaSBzb3Jh")
+st.sidebar.link_button("Submit Feedback about the output","https://forms.gle/c2zuB8DTgRcKY2QKA")
 
-text = st.text_input('Put your texts here')
-if st.button('Run NER'):
-    st.write("""1. Project names: AI/ML deployment
-2. Risks: potential risk that can slow down the launch date, scalability issue in db transfer, latency of API calls causing bottleneck
-3. Who raised the risk: Anant
-4. Which team is assigned to solve the risk: not mentioned
-5. Mitigation plan: not mentioned
-6. Key Decisions: meeting at 1:00pm to decide about what can be done with minimum development time
-7. Rationale: not mentioned
-8. Who raised the decision: Anant
-9. Relevant Updates: not mentioned
-10. Achievements: tech team achieving their quarterly targets
-11. Priorities: not mentioned
-12. Important dates and timeframes: not mentioned
-13. Links to important resources: https://docs.google.com/document/d/1v4Bxwe_gdkmwPxZ3jaBsp4IpjsC703CFaMODl017stE/edit""")
+
+text = st.text_area('Enter your text here',height = 300)
+if st.button('Generate output'):
+    st.write(""""Project XYZ:
+  - Risks:
+    Description: Key dependency on the ABC project, specifically in terms of data integration. Delays in the ABC project have impacted the timelines of the XYZ project.
+    Raised by:  John Smith (Product Marketing Manager)
+    Raised on: 16/02/2024
+    Mitigation plan: Issue already escalated to project ABC's project manager. Escalate the issue further within the organization and seek assistance from Sarah Johnson
+    Assigned to: N/A
+
+  - Decision:
+
+    Decision description: N/A
+    Decision Rationale: N/A
+    Who took the decision: N/A
+    Raised on: N/A
+
+  - Updates:
+
+    Update Description: The XYZ project has achieved significant milestones, including the successful launch of the beta version and positive feedback from early users.
+    Team: Product Marketing
+    Next week's priorities: Address critical user interface issues reported during the beta phase, conduct user research for future improvements
+    When: 16/02/2024
+
+  - Link to important resources:
+
+    Important resources: XYZ project documentation
+    Link: [XYZ Project Documentation](https://www.example.com/xyz-project-documentation)"""")
 else:
     pass
